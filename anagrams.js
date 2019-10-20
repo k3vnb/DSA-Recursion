b9ab8cf7c33143611a323eb530788db46e36ab77
Kevin Boyle <papershack@gmail.com>
2019-10-20 01:27:45 -0700

    complete orgChart algorithm challenge

--- anagrams.js
+++ anagrams.js
@@ -2,6 +2,7 @@
 
 // Hint: For your algorithm, you might want to think about a prefix and use that to create the new words. For example, given "east", use "e" as a prefix and place it in front of all 6 permutations of "ast" — "ast", "ats", "sat", "sta", "tas", and "tsa". This will give you the words "east", "eats", "esat", "esta", "etas", and "etsa". Continue this way until you find all the anagrams for "east". Then you can use "a" as a prefix and permute the remaining words "est". For "east", there should be 24 words.
 
+// helper function
 function swap(charArr, i, j){
     let temp = charArr[i]
     charArr[i] = charArr[j]
@@ -11,15 +12,20 @@ function swap(charArr, i, j){
 
 function findAnagrams(chars, currentIndex = 0){
     let charArr
+    //first iteration only receives a string, further iterations receive an arr
     typeof chars === "string" ? charArr = chars.split('') : charArr = chars
+    //base case proxy, prints the joined arr as a string when it is at the last letter, hence last swap opportunity
     if (currentIndex === charArr.length - 1){
         console.log(charArr.join(''))
     }
     for (let i = currentIndex; i < charArr.length; i++){
+        //swap current index w/ i
         swap(charArr, currentIndex, i)
+        //recursively takes the swapping beginning with the next letter in the character array
         findAnagrams(charArr, currentIndex + 1)
+        //backtracks the last swap 
         swap(charArr, currentIndex, i)
     }
 }
 
-findAnagrams('boyle')
+findAnagrams('boats')
